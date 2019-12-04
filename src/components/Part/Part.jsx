3d5ae6ab24fc2ccef7ee585f90d685a53b7e378e
Grant <gmontgomery1994@gmail.com>
2019-12-03 22:17:42 -0800

    beginnings of the dropdown trying to create the right way.

---
+++ src/components/Part/Part.jsx
@@ -0,0 +1,17 @@
+import React, { Component } from "react";
+import "./Part.css";
+
+class Part extends Component {
+  state = {};
+  render() {
+    return (
+      <div className="part-wrapper">
+        <div className="part-image-wrapper">
+          <img src={this.props.Part.image} alt="" />
+        </div>
+      </div>
+    );
+  }
+}
+
+export default Part;
--- src/components/Parts/Parts.jsx
+++ src/components/Parts/Parts.jsx
@@ -1,27 +1,52 @@
 import React, { Component } from "react";
 import { AppContext } from "../../AppContext";
+import { Part } from "../Part";
 import "./Parts.css";
 
 class Parts extends Component {
-  state = {};
+  state = {
+    dropdown: false
+  };
+
+  triggerDropdown = event => {
+    event.preventDefault();
+    let { dropdown } = this.state;
+    dropdown === false
+      ? this.setState({ dropdown: true })
+      : this.setState({ dropdown: false });
+  };
+
+  renderDropdown = list => {
+    let { dropdown } = this.state;
+    if (dropdown === false) {
+      return (
+        <div className="parts-dropdown">
+          <ul>
+            {list.Parts.map(part => (
+              <li>
+                <Part Part={part}></Part>
+              </li>
+            ))}
+          </ul>
+        </div>
+      );
+    } else {
+      return <React.Fragment></React.Fragment>;
+    }
+  };
 
   render() {
+    console.log(this.state.dropdown);
     return (
       <AppContext.Consumer>
         {value => (
-          <div className="parts-box">
-            <div className="parts-dropdown">
-              <ul>
-                {value.Parts.map(part => (
-                  <li>
-                    <div className="part-image-wrapper">
-                      <img src={part.image} alt="" />
-                    </div>
-                  </li>
-                ))}
-              </ul>
-            </div>
-          </div>
+          <React.Fragment>
+            <div
+              className="parts-box"
+              onClick={e => this.triggerDropdown(e)}
+            ></div>
+            {this.renderDropdown(value)}
+          </React.Fragment>
         )}
       </AppContext.Consumer>
     );
