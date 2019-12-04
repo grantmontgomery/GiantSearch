991c31cbffbec978f655b2daa20a4a97829451da
Grant <gmontgomery1994@gmail.com>
2019-12-04 15:38:21 -0800

    There's now a button which shows up next to Part component which deletes it from list.

--- src/components/Part/Part.jsx
+++ src/components/Part/Part.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from "react";
+import { AppContext } from "../../AppContext";
 import "./Part.css";
 
 class Part extends Component {
@@ -7,25 +8,28 @@ class Part extends Component {
   renderType = () => {
     if (this.props.Part.type === "venue") {
       return (
-        <div className="part-wrapper">
-          <div className="part-image-wrapper">
-            <img src={this.props.Part.image} alt="" />
-          </div>
-          <div className="part-text-wrapper">
-            <span> {this.props.Part.name}</span>
-            <br />
-            <ul>
-              <li>{this.props.Part.location.city}</li>
-              <li>{this.props.Part.price}</li>
-            </ul>
-
-            {/* <ul>
-              <li>{this.props.Part.name}</li>
-              <li>{this.props.Part.location.city}</li>
-              <li>{this.props.Part.price}</li>
-            </ul> */}
-          </div>
-        </div>
+        <AppContext.Consumer>
+          {value => {
+            return (
+              <div className="part-wrapper">
+                <div className="part-image-wrapper">
+                  <img src={this.props.Part.image} alt="" />
+                </div>
+                <div className="part-text-wrapper">
+                  <span>{this.props.Part.name}</span>
+                  <br />
+                  <span>{this.props.Part.location.city}</span>
+                  <br />
+                  <span>{this.props.Part.price}</span>
+                </div>
+                <button
+                  className="remove-part"
+                  onClick={() => value.removePart(this.props.Part.id)}
+                ></button>
+              </div>
+            );
+          }}
+        </AppContext.Consumer>
       );
     } else {
       return (
